be1f5b114d9ec6d1de2cc94a53d0239b79282dbf
Adrian <wegrzyn.adr@gmail.com>
2018-12-18 21:12:49 +0100

    diet plan

--- src/diet/dietWeek/DietWeek.js
+++ src/diet/dietWeek/DietWeek.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
-import {StyleSheet, View } from 'react-native';
-
-
+import {StyleSheet, View, ScrollView, TouchableOpacity} from 'react-native';
+import {Text} from "react-native-elements";
 
 
 export default class App extends Component<> {
 
+    nameDay = (day) => {
+        let arr = day.split('-')
+        var days = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
+        var d = new Date(arr[2], arr[1]-1, arr[0]);
+        return  days[d.getDay()];
+    };
+
     render() {
         return (
-            <View style={styles.container}>
-
-            </View>
+            <ScrollView style={styles.container}>
+                <Text style={styles.dateText}>12-12-2018</Text>
+                <TouchableOpacity style={styles.dayButton}>
+                    <Text style={styles.buttonText}>{this.nameDay('12-12-2018')}</Text>
+                </TouchableOpacity>
+                <Text style={styles.dateText}>21/10/2014</Text>
+                <TouchableOpacity style={styles.dayButton}>
+                    <Text style={styles.buttonText}>{this.nameDay('12-12-2018')}</Text>
+                </TouchableOpacity>
+                <Text style={styles.dateText}>21/10/2014</Text>
+                <TouchableOpacity style={styles.dayButton}>
+                    <Text style={styles.buttonText}>{this.nameDay('12-12-2018')}</Text>
+                </TouchableOpacity>
+                <Text style={styles.dateText}>21/10/2014</Text>
+                <TouchableOpacity style={styles.dayButton}>
+                    <Text style={styles.buttonText}>{this.nameDay('12-12-2018')}</Text>
+                </TouchableOpacity>
+                <Text style={styles.dateText}>21/10/2014</Text>
+                <TouchableOpacity style={styles.dayButton}>
+                    <Text style={styles.buttonText}>{this.nameDay('12-12-2018')}</Text>
+                </TouchableOpacity>
+                <Text style={styles.dateText}>21/10/2014</Text>
+                <TouchableOpacity style={styles.dayButton}>
+                    <Text style={styles.buttonText}>{this.nameDay('12-12-2018')}</Text>
+                </TouchableOpacity>
+            </ScrollView>
         );
     }
 }
@@ -19,4 +48,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    dayButton: {
+        marginHorizontal: 15,
+        marginVertical: 10,
+        borderWidth: 3,
+        borderColor: '#000000',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        paddingVertical: 40
+    },
+    buttonText: {
+        fontSize: 30,
+    },
+    dateText: {
+        justifyContent: 'center',
+        textAlign: 'center',
+        marginVertical: 5,
+        fontSize: 25,
+        color: '#7E7E7E'
+    }
 });
